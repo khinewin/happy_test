2a832d45c59036cf683cba50cd5d206e52c947b5
kosithu-kw <kosithu.20@gmail.com>
2022-12-20 14:30:13 +0630

    fix og 7

--- resources/views/share.blade.php
+++ resources/views/share.blade.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
 
-    <meta property="og:title"              content="{{$share->share_name}}" />
-
+    <meta property="og:title"              content="This is my title" />
     <meta property="og:url"                content="http://happy.mmdata.cc/share/{{$share->post_id}}/{{$share->share_id}}/{{$share->img_id}}/" />
     <meta property="og:type"               content="article" />
-    <meta property="og:description"        content="{{$share->share_name}}" />
+    <meta property="og:description"        content="THis is my desc" />
     <meta property="og:image"              content= "https://happy.mmdata.cc/items/{{$share->post_img}}" />
     <meta property="fb:app_id" content="671829351269589"/>
 
