eef054826427bd45c8f5a88475ba6d7e0bb9d0be
kosithu-kw <kosithu.20@gmail.com>
2022-12-20 15:22:01 +0630

    fix og 17

--- resources/views/share.blade.php
+++ resources/views/share.blade.php
@@ -9,7 +9,8 @@
     <meta property="og:type"               content="article" />
     <meta property="og:url"                content="http://happy.mmdata.cc/share/{{$share->post_id}}/{{$share->share_id}}/{{$share->img_id}}/" />
     <meta name="keywords" content="{{$share->share_name}}">
-    <meta property="og:title"              content="{!! htmlspecialchars($share->share_name)!!}" />
+    <meta property="og:site_name" content={{$share->share_name}}/>
+    <meta property="og:title"              content={!! htmlspecialchars($share->share_name)!!} />
     <meta property="og:description"        content="{!! htmlspecialchars($share->share_name) !!}" />
     <meta property="og:image"              content= "https://happy.mmdata.cc/items/{{$share->post_img}}" />
     <meta property="fb:app_id" content="671829351269589"/>
