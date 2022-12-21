2fe77c8973d00c0d7f1a6b8f55a40954e226a365
KhineWin <khinewin123@gmail.com>
2022-12-21 08:59:19 +0630

    fix sharename4

--- resources/views/share.blade.php
+++ resources/views/share.blade.php
@@ -20,6 +20,9 @@
     
 </head>
 <body>
+    <div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v15.0&appId=671829351269589&autoLogAppEvents=1" nonce="ADpohHBV"></script>
+
     @include('partials.navbar')
  
     <div class="container mt-2">
@@ -43,9 +46,10 @@
                                <a href="{{route('post.show',['id'=>$share->post_id])}}"  wire:model="doRetry" class="btn btn-primary btn-lg mb-2" ><i class="fa-solid fa-repeat"></i> Retry</a>
                               {{--<a target="_blank" class="btn btn-primary mb-2 btn-lg" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fhappy.mmdata.cc%2Fshare%2F{{$share->post_id}}%2F{{$share->share_id}}%2F{{$share->img_id}}&amp;src=sdkpreparse"><i class="fa-brands fa-facebook"></i> Share On Facebook</a> --}}
 
-                              <div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v15.0&appId=671829351269589&autoLogAppEvents=1" nonce="ADpohHBV"></script>
-<div class="fb-share-button" data-href="https://happy.mmdata.cc/share/{{$share->post_id}}/{{$share->share_id}}/{{$share->img_id}}?share_name={{$share_name}}" data-layout="button" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fhappy.mmdata.cc%2Fshare%2F{{$share->post_id}}%2F{{$share->share_id}}%2F88%3Fshare_name%3D{{$share_name}}&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore"><i class="fa-brands fa-facebook"></i> Share On Facebook</a></div>
+                              
+                            <div class="fb-share-button btn btn-lg mb-2" data-href="https://happy.mmdata.cc/share/{{$share->post_id}}/{{$share->share_id}}/{{$share->img_id}}?share_name={{$share_name}}" data-layout="button" data-size="large">
+                                <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fhappy.mmdata.cc%2Fshare%2F{{$share->post_id}}%2F{{$share->share_id}}%2F88%3Fshare_name%3D{{$share_name}}&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a>
+                            </div>
                                <a class="btn btn-primary mb-2 btn-lg" href="{{route('/')}}"><i class="fa-solid fa-house"></i> Home</a>
 
                             </div>
