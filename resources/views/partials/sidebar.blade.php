e6d55be36c41b0472c67da9080ede0e174e934dd
KhineWin <khinewin123@gmail.com>
2022-12-21 14:26:12 +0630

    fix icon

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="mt-5 d-grid gap-2">
-    <a href="{{route('dashboard')}}" class="btn btn-outline-primary"> Dashboard</a>
-    <a href="{{route('posts.new')}}" class=" btn btn-outline-primary">New Post</a>
+    <a href="{{route('dashboard')}}" class="btn btn-outline-primary"><i class="fa-solid fa-gauge"></i> Dashboard</a>
+    <a href="{{route('posts.new')}}" class=" btn btn-outline-primary"><i class="fa-solid fa-message-plus"></i> New Post</a>
 
-    <a href="{{route('posts')}}" class=" btn btn-outline-primary">Posts</a>
+    <a href="{{route('posts')}}" class=" btn btn-outline-primary"><i class="fa-solid fa-folder-grid"></i> Posts</a>
     <a class="btn btn-outline-primary" href="{{route('logout')}}"><i class="fa-solid fa-arrow-right-from-bracket"></i> Logout</a>
 </div>
