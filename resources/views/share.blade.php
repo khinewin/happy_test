021bef41a2c1904038f6a4238065222b0b0e4c43
KhineWin <khinewin123@gmail.com>
2022-12-21 08:43:56 +0630

    fix sharename3

--- resources/views/share.blade.php
+++ resources/views/share.blade.php
@@ -9,7 +9,7 @@
     <meta property="profile:username" content="{{$share_name}}"/>
     <meta property="og:url"                content="{{url()->current()}}?share_name={{$share_name}}" />
     <meta property="og:title"              content="@if($share_name){{$share_name}}@else{{$share->post_content}}@endif" />
-    <meta property="og:description"        content="ပျော်ရွှင်စရာကောင်းသော အချိန်လေးပိုင်ဆိုင်နိုင်ကြပါစေ။" />
+    <meta property="og:description"        content="{{$share->post_content}}" />
     <meta property="og:image"              content= "https://happy.mmdata.cc/items/{{$share->post_img}}" />
     <meta property="fb:app_id" content="671829351269589"/>
     <link rel="canonical" href="{{url()->current()}}?share_name={{$share_name}}"/>
@@ -41,7 +41,11 @@
 
 
                                <a href="{{route('post.show',['id'=>$share->post_id])}}"  wire:model="doRetry" class="btn btn-primary btn-lg mb-2" ><i class="fa-solid fa-repeat"></i> Retry</a>
-                               <a target="_blank" class="btn btn-primary mb-2 btn-lg" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fhappy.mmdata.cc%2Fshare%2F{{$share->post_id}}%2F{{$share->share_id}}%2F{{$share->img_id}}&amp;src=sdkpreparse"><i class="fa-brands fa-facebook"></i> Share On Facebook</a>
+                              {{--<a target="_blank" class="btn btn-primary mb-2 btn-lg" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fhappy.mmdata.cc%2Fshare%2F{{$share->post_id}}%2F{{$share->share_id}}%2F{{$share->img_id}}&amp;src=sdkpreparse"><i class="fa-brands fa-facebook"></i> Share On Facebook</a> --}}
+
+                              <div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v15.0&appId=671829351269589&autoLogAppEvents=1" nonce="ADpohHBV"></script>
+<div class="fb-share-button" data-href="https://happy.mmdata.cc/share/{{$share->post_id}}/{{$share->share_id}}/{{$share->img_id}}?share_name={{$share_name}}" data-layout="button" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fhappy.mmdata.cc%2Fshare%2F{{$share->post_id}}%2F{{$share->share_id}}%2F88%3Fshare_name%3D{{$share_name}}&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore"><i class="fa-brands fa-facebook"></i> Share On Facebook</a></div>
                                <a class="btn btn-primary mb-2 btn-lg" href="{{route('/')}}"><i class="fa-solid fa-house"></i> Home</a>
 
                             </div>
