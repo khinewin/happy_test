dfe434f99e7700eba84dde16e10f7f8521080339
KhineWin <khinewin123@gmail.com>
2022-12-21 09:52:39 +0630

    fix footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="card">
+<footer class="card mt-5">
     <div class="card-footer text-center">
             <a href="/" class="btn btn-link" style="text-decoration: none">&copy; ပျော်ပါစေ 2022</a>
     </div>
